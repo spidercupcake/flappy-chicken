c99e5869cf6a41021cdb9659023b8c8066838d60
spidercupcake <xmze11690911300@gmail.com>
2024-09-20 21:47:37 +0500

    Added jump functionality to the chicken character.
    - Implemented basic jump logic for Cojiro, allowing it to jump by 500px when clicked.
    - The chicken continues to fall from the top of the screen to the bottom when not jumping.
    - Optimized the `IsJumping` state to handle the jump trigger and reset after each jump.
    - Maintained continuous falling behavior when Cojiro is not jumping.
    - Fixed the chicken code asset to use `/assets/cojiro.svg` for consistency with the character name.

--- src/components/Chicken.tsx
+++ src/components/Chicken.tsx
@@ -2,27 +2,38 @@
 import { useEffect, useState } from "react";
 
 export default function Chicken() {
-  const [position, setPosition] = useState(1);
+  const [position, setPosition] = useState(0);
+  const [IsJumping, setIsJumping] = useState(false);
+
   const speed = 2;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPosition((prevPosition) => {
-        if (prevPosition < window.innerHeight - 80) return prevPosition + speed;
-        return prevPosition;
-      });
+      if (!IsJumping) {
+        setPosition((prevPosition) => {
+          if (prevPosition < window.innerHeight - 80)
+            return prevPosition + speed;
+          return prevPosition;
+        });
+      } else {
+        setPosition((prevPosition) => {
+          return prevPosition - 500;
+        });
+        setIsJumping(false);
+      }
     }, 16);
     return () => clearInterval(interval);
-  }, []);
+  }, [IsJumping]);
 
   return (
     <div
+      onClick={() => setIsJumping(true)}
       className="absolute h-20 w-20"
       style={{
         top: `${position}px`,
       }}
     >
-      <img src="/assets/chicken.svg" alt="CHICKEN" />
+      <img src="/assets/cojiro.svg" alt="CHICKEN" />
     </div>
   );
 }
